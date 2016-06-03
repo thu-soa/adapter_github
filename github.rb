7ecacb790c153dbe2ad001ab19fb93fffbe6775c
Alex Wang <ice_b0und@hotmail.com>
2016-06-03 15:05:01 +0800

    remove id field

--- github.rb
+++ github.rb
@@ -3,6 +3,7 @@ require './main'
 require './errors'
 require 'json'
 require 'sinatra/cross_origin'
+require 'http'
 
 configure do
   enable :cross_origin
@@ -10,6 +11,19 @@ end
 
 after do
   ActiveRecord::Base.connection.close
+end
+def get_user_by_token(token) 
+  response = HTTP.get(URI("#{USER_INFO_URL}?token=#{token}")) 
+    begin 
+      json = JSON.parse(response) 
+        if json['status'] == 'ok' 
+          json 
+        else 
+          nil 
+        end 
+    rescue JSON::JSONError 
+      raise 'auth returns garbage' 
+    end 
 end
 
 post '/api/v1/register' do
@@ -50,7 +64,9 @@ end
 
 get '/api/v1/check_registered' do
   begin
-    uid, token = params.fetch_values('id', 'token')
+    token = params.fetch_values('token').first
+    user = get_user_by_token(token)
+    uid = user['id']
   rescue KeyError
     er 'parameter error'
   end
